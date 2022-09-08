f570ba3f8f8ca348be2fb5e7f4f1c40432162109
Buabeng Emmanuel Aikins <67046324+guenews@users.noreply.github.com>
2022-09-08 21:39:17 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <unisted.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return:1
  */
 int main(void)
 {
-	printf("Holberton School");
-	return (0);
+	write(2,"and that pieceof art is useful\" -dora korpar,2015-10-19\n",59);
+	return (1);
 }
